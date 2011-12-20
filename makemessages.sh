e2bd88bdeddae8a7ce440df01a9da8e11bec1e6a
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2011-12-20 11:28:23 +0200

    processing js files in makemessages

--- makemessages.sh
+++ makemessages.sh
@@ -10,6 +10,8 @@ lang_list=$(echo ${2} | tr "," "\n")
 for lang in $lang_list
 do
     ./manage.py makemessages -l $lang -e html,txt --ignore=${1}-venv/*
+    ./manage.py makemessages -l $lang -e html,js --ignore=${1}-venv/* -d django
+    
 done
 ./manage.py compilemessages
 
