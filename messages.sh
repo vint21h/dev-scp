c15ecdd3842a6f2d65b57883270d27f7633fa483
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2015-09-10 10:02:29 +0300

    some messages.sh fixes, remove old useless makemessages.sh

--- messages.sh
+++ messages.sh
@@ -3,29 +3,28 @@
 # dev-scp
 # messages.sh
 
-# WARNING: This script MUST run from project root directory.
+# WARNING: This script MUST run only from project root directory.
 # Require two positioned args:
-#     first: project name (default - get from project 'meta/name' or from basename).
-#     second: action (default - makemessages), possible variants: makemessages, compilemessages. Makemessages always run compilemessages.
+#     first: action (default - makemessages), possible variants: makemessages, compilemessages.
+#     second: project name (default - get from project 'META/name.txt' or from basename).
 # Also support more options:
-#     third: apps (default - getting from project 'meta/apps'): list of comma separated project applications names.
-#     fourth: languages (default - getting from project 'meta/languages'): list of comma separated project languages.
-#     five: python path to project settings file.
-# Require project 'meta/name' file.
+#     third: apps (default - getting from project 'META/apps.txt'): list of comma separated project applications names.
+#     fourth: languages (default - getting from project 'META/languages.txt'): list of comma separated project languages.
+#     five: python path to project settings module (default - $NAME.settings.dev).
 
-# getting global variables
+# global variables
 PPATH=$PWD  # project path
+ACTION=${1:-'makemessages'}
 if [ -f $PWD/META/name.txt ]; then
-    NAME=$(echo ${1:-`cat $PWD/META/name.txt | tr -d "\n"`})
+    NAME=$(echo ${2:-`cat $PWD/META/name.txt | tr -d "\n"`})
 else
     NAME=`basename $PWD`
 fi
-MANAGE=$PPATH/manage.py
-ARCH=`arch`
-ACTION=${2:-'makemessages'}
 APPS=$(echo ${3:-`cat $PPATH/META/apps.txt`} | tr -d "\n" | tr "," "\n")
 LANGUAGES=$(echo ${4:-`cat $PPATH/META/languages.txt`} | tr -d "\n" | tr "," "\n")
 SETTINGS=${5:-$NAME.settings.dev}
+MANAGE=$PPATH/manage.py
+ARCH=`arch`
 
 # enable virtualenv
 source $PPATH/.env/$ARCH/bin/activate
