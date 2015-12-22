bb8090bec4a9f65a15bccf3a1a20f09ea3130591
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2015-12-22 04:02:02 +0200

    improve mail-server.sh

--- mail-server.sh
+++ mail-server.sh
@@ -3,4 +3,11 @@
 # dev-scp
 # mail-server.sh
 
-python -m smtpd -n -c DebuggingServer localhost:${1}
+# Debugging mail server.
+# Receive two positioned args: host and port.
+
+HOST=${1:-'127.0.0.1'}
+PORT=${1:-'1025'}
+
+# run python SMTP server
+python -m smtpd -n -c DebuggingServer $HOST:$PORT
